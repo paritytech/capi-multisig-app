8c2787d47a08c5c3beb43ed02c41051c95ab6abe
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-02-03 16:22:42 +0700

    fmt

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,14 +2,14 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/db_write.ts";
-import * as $1 from "./routes/create-multisig.tsx";
-import * as $2 from "./routes/create-transaction.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/transactions.tsx";
-import * as $$0 from "./islands/CreateMultisig.tsx";
-import * as $$1 from "./islands/WalletConnect.tsx";
+import config from "./deno.json" assert { type: "json" }
+import * as $$0 from "./islands/CreateMultisig.tsx"
+import * as $$1 from "./islands/WalletConnect.tsx"
+import * as $0 from "./routes/api/db_write.ts"
+import * as $1 from "./routes/create-multisig.tsx"
+import * as $2 from "./routes/create-transaction.tsx"
+import * as $3 from "./routes/index.tsx"
+import * as $4 from "./routes/transactions.tsx"
 
 const manifest = {
   routes: {
@@ -25,6 +25,6 @@ const manifest = {
   },
   baseUrl: import.meta.url,
   config,
-};
+}
 
-export default manifest;
+export default manifest
