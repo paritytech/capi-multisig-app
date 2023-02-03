ed3a2bd5654f856ec38fae45c2a655f3fd25121d
Harry Solovay <harrysolovay@gmail.com>
2023-02-02 19:54:03 -0500

    attempt to fix deploy

--- components/layout/mod.tsx
+++ components/layout/mod.tsx
@@ -1,17 +1,4 @@
-import type { ComponentChildren } from "preact"
-import { Head } from "./Head.tsx"
-
 export * from "./Head.tsx"
 export * from "./Header.tsx"
+export * from "./Layout.tsx"
 export * from "./Navbar.tsx"
-
-export const Layout = ({ children }: { children: ComponentChildren }) => {
-  return (
-    <>
-      <Head />
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 py-4">
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
