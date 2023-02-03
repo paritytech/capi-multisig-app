1ff206b39af29eb0c3dbae2b3c8c5798c763ab48
Harry Solovay <harrysolovay@gmail.com>
2023-02-02 20:46:14 -0500

    more debugging

---
+++ _tasks/star.ts
@@ -0,0 +1,17 @@
+import * as fs from "std/fs/mod.ts"
+import * as path from "std/path/mod.ts"
+
+let generated = ""
+for await (
+  const entry of fs.walk(".", {
+    match: [/\.ts$/],
+    skip: [/^target\//],
+  })
+) {
+  generated += `import ${JSON.stringify(`../${entry.path}`)};\n`
+}
+
+const dir = path.join(Deno.cwd(), "target")
+await fs.ensureDir(dir)
+const dest = path.join(dir, "star.ts")
+await Deno.writeTextFile(dest, generated)
--- components/layout/mod.tsx
+++ components/layout/mod.tsx
@@ -1,4 +1,4 @@
 export * from "./Head.tsx"
 export * from "./Header.tsx"
-// export * from "./Layout.tsx"
+export * from "./Layout.tsx"
 export * from "./Navbar.tsx"
