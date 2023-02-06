4ead207bc88821804a95b46774c9c93b64825236
tjjfvi <t6@t6.fyi>
2023-02-06 08:03:43 -0500

    fix deploy script

---
+++ _tasks/deploy_import_map.ts
@@ -0,0 +1,6 @@
+
+const importMap = JSON.parse(Deno.readTextFileSync("import_map.json"))
+
+importMap.imports["http://localhost:4646/"] = "./target/capi/"
+
+Deno.writeTextFileSync("import_map.json", JSON.stringify(importMap, 2))
