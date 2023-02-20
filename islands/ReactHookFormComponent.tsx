06b6755d3d6291e99b812430e89945e0ae6da165
tamrai <nick.zbiegien@parity.io>
2023-02-20 15:51:26 +0700

    zod

--- islands/ReactHookFormComponent.tsx
+++ islands/ReactHookFormComponent.tsx
@@ -3,16 +3,22 @@
 // import { useForm } from "https://esm.sh/react-hook-form@7.43.1?external=react,react-dom,@types/react,@types/react-dom"
 // import { useForm } from "https://esm.sh/react-hook-form@7.43.1"
 // import { useForm } from "react-hook-form"
-import { yupResolver } from "https://esm.sh/@hookform/resolvers@2.9.11/yup";
-import { useForm } from "https://esm.sh/react-hook-form@7.43.1?external=react,react-dom,@types/react,@types/react-dom";
-import * as yup from "https://esm.sh/yup@1.0.0";
+import { zodResolver } from "https://esm.sh/@hookform/resolvers@2.9.11/zod"
+import { useForm } from "https://esm.sh/react-hook-form@7.43.1?external=react,react-dom,@types/react,@types/react-dom"
+// import * as yup from "https://esm.sh/yup@1.0.0"
+import * as z from "https://esm.sh/zod@3.20.6"
 
-const schema = yup
-  .object({
-    firstName: yup.string().required(),
-    age: yup.number().positive().integer().required(),
-  })
-  .required();
+// const schema = yup
+//   .object({
+//     firstName: yup.string().required(),
+//     age: yup.number().positive().integer().required(),
+//   })
+//   .required()
+
+const schema = z.object({
+  firstName: z.string().min(1, { message: "Required" }),
+  age: z.number().min(10),
+})
 
 export default function ReactHookFormComponent() {
   const {
@@ -20,19 +26,23 @@ export default function ReactHookFormComponent() {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const onSubmit = (data) => console.log(data);
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit = (data, event) => {
+    event.preventDefault()
+    console.log(data)
+  }
 
   return (
     <form className="m-5" onSubmit={handleSubmit(onSubmit)}>
       <input {...register("firstName")} />
       <p>{errors.firstName?.message}</p>
 
-      <input {...register("age")} />
+      <input type="number" {...register("age", { valueAsNumber: true })} />
       <p>{errors.age?.message}</p>
 
       <input type="submit" />
     </form>
-  );
+  )
 }
