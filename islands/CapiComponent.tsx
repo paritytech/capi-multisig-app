8d2ca0a7f376ab832ab6ed41aaf9766cb95b7ab6
tamrai <nick.zbiegien@parity.io>
2023-02-24 17:08:41 +0700

    deploy fix

--- islands/CapiComponent.tsx
+++ islands/CapiComponent.tsx
@@ -1,10 +1,10 @@
-import { System } from "http://localhost:4646/frame/wss/rpc.polkadot.io/@v0.9.37/mod.ts"
+// import { System } from "http://localhost:4646/frame/wss/rpc.polkadot.io/@v0.9.37/mod.ts"
 import { useEffect } from "preact/hooks"
 
 export default function CapiComponent() {
   useEffect(() => {
     ;(async () => {
-      console.log(await System.Account.keyPage(10).run())
+      // console.log(await System.Account.keyPage(10).run())
     })()
   }, [])
   return <div>Capi-using component</div>
