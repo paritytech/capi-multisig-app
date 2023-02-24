2f5955130a675acf50ba64c59f0f822fabc2f8df
tamrai <nick.zbiegien@parity.io>
2023-02-24 17:31:18 +0700

    bg fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,9 @@ export default function Landing() {
       <Header />
       <Navbar title={<h1 className="text-2xl">Overview</h1>} />
       <div className="flex flex-col items-center justify-center mt-10 gap-4">
-        <WizardApp />
+        <Card className="flex flex-col items-center w-[600px]">
+          <WizardApp />
+        </Card>
         <Card className="flex flex-col items-center w-[600px]">
           <p>None of the accounts connected is a part of a multisig.</p>
           <p>You can create a new multisig from the top toolbar.</p>
