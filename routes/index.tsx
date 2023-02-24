c68fd6f619af128c50261cbda24858d2e069a140
tamrai <nick.zbiegien@parity.io>
2023-02-24 17:04:57 +0700

    wizard init

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import { Card, Header, Layout, Navbar } from "components"
 import CapiComponent from "islands/CapiComponent.tsx"
+import WizardApp from "islands/WizardApp.tsx"
 
 export default function Landing() {
   return (
@@ -7,6 +8,7 @@ export default function Landing() {
       <Header />
       <Navbar title={<h1 className="text-2xl">Overview</h1>} />
       <div className="flex flex-col items-center justify-center mt-10 gap-4">
+        <WizardApp />
         <Card className="flex flex-col items-center w-[600px]">
           <p>None of the accounts connected is a part of a multisig.</p>
           <p>You can create a new multisig from the top toolbar.</p>
