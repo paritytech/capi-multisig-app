2634cb38f2ec813b000a241c37a52f5d91922a31
Andreea Eftene <hi@pixelle.ro>
2023-03-01 11:42:36 +0100

    clean slate

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { start } from "$fresh/server.ts"
-import manifest from "./fresh.gen.ts"
-
-import twindPlugin from "$fresh/plugins/twind.ts"
-import twindConfig from "./twind.config.ts"
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] })
