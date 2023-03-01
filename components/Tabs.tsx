a33cd3dc08d926f2256ada244873dc056dec4d4e
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-01 20:12:21 +0800

    fix Tabs colours

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -17,7 +17,7 @@ export function Tabs({ tabs }: {
         <select
           id="tabs"
           name="tabs"
-          className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
           defaultValue={tabs.find((tab) => tab.current)!.name}
         >
           {tabs.map((tab) => <option key={tab.name}>{tab.name}</option>)}
@@ -32,7 +32,7 @@ export function Tabs({ tabs }: {
                 href={tab.href}
                 className={classNames(
                   tab.current
-                    ? "border-pink-500 text-pink-600"
+                    ? "border-pink-500 text-black"
                     : "border-transparent text-gray-500 hover:border-pink-500 hover:text-gray-700",
                   "whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium",
                 )}
