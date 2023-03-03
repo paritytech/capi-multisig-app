3d7451d8d739f9f7e3ee8422b16603adeebef0f8
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-04 00:16:43 +0800

    identicon

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import { CenteredCard } from '../components/CenteredCard'
 import { Link } from 'react-router-dom'
 import { Button } from '../components/Button'
 import { IconPlus } from '../components/icons/IconPlus'
+import { Identicon } from '../components/identicon/Identicon'
 
 export function Dashboard() {
   return (
@@ -13,6 +14,18 @@ export function Dashboard() {
             <Button iconLeft={<IconPlus />}>New multisig</Button>
           </div>
         </Link>
+        <Identicon
+          size={32}
+          value="5EHufKvrjg3QGn4VFgFpSq7fybnLauire1ULHEcosAg8E47X"
+        />
+        <Identicon
+          size={32}
+          value="5EoB2vbYV3MtdcnaaEFqisGY8eERi3ttZ8ytQkYcVhBqV87u"
+        />
+        <Identicon
+          size={32}
+          value="5EP1x5VUMwQiaWHkNa5d6qE2FqRW8Apyw6P67NPUzzHMdDz4"
+        />
       </CenteredCard>
     </Page>
   )
