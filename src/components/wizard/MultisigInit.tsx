a2bae0da4ef2014dabe71ba2f2b5d0f733c76b21
tamrai <nick.zbiegien@parity.io>
2023-03-10 17:59:51 +0700

    feat(ui): Wizard (#78)

---
+++ src/components/wizard/MultisigInit.tsx
@@ -0,0 +1,66 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { signal } from '@preact/signals'
+import type { Signal } from '@preact/signals'
+
+import { Button } from '../Button'
+import { useWizardNavigation, useWizardFormDataStep } from './Wizard'
+
+export const multisigInitSchema = z.object({
+  name: z.string().min(1, { message: 'Required a multisig name' }),
+})
+export type MultisigInitEntity = z.infer<typeof multisigInitSchema>
+
+export function createDefaultMultisigInit(): Signal<MultisigInitEntity> {
+  return signal({
+    name: '',
+  })
+}
+
+export function MultisigInit() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MultisigInitEntity>({
+    resolver: zodResolver(multisigInitSchema),
+    mode: 'onChange',
+  })
+  const { goNext } = useWizardNavigation()
+  const { formDataStep, updateFormDataStep } =
+    useWizardFormDataStep<MultisigInitEntity>()
+
+  const onSubmit = (formDataNew: MultisigInitEntity) => {
+    updateFormDataStep(formDataNew)
+    goNext()
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1 class="text-xl leading-8">1. Multisig setup</h1>
+      <hr class="border-t border-gray-300 mt-6 mb-4" />
+      <label class="leading-6">
+        Multisig Name: <span class="text-pink-600">*</span>
+      </label>
+      <input
+        {...register('name')}
+        id="name"
+        defaultValue={formDataStep.name}
+        placeholder="Enter the name..."
+        class="block rounded-lg border border-gray-300 p-2 my-2 w-1/2"
+      />
+      {errors.name && (
+        <div class="bg-red-100 text-red-700 p-2 rounded mt-2 border border-red-300">
+          {errors.name.message}
+        </div>
+      )}
+      <hr class="divide-x-0 divide-gray-300 mt-4 mb-2" />
+      <div class="flex justify-end">
+        <Button variant="ghost" type="submit">
+          Next &gt;
+        </Button>
+      </div>
+    </form>
+  )
+}
---
+++ src/components/wizard/Wizard.tsx
@@ -0,0 +1,74 @@
+import { toChildArray } from 'preact'
+import type { ComponentChildren } from 'preact'
+import { signal, computed } from '@preact/signals'
+
+import { createDefaultMultisigInit } from './MultisigInit'
+import type { MultisigInitEntity } from './MultisigInit'
+import { createDefaultMembers } from './MultisigMembers'
+import type { MultisigMemberEntity } from './MultisigMembers'
+import { createDefaultFund } from './MultisigFund'
+import type { MultisigFundEntity } from './MultisigFund'
+
+export const multisigSteps = ['init', 'members', 'fund', 'summary'] as const
+
+export type MultisigEntities =
+  | MultisigInitEntity
+  | MultisigMemberEntity
+  | MultisigFundEntity
+
+export type FormData = ReturnType<typeof createDefaultFormData>
+
+function createDefaultFormData() {
+  return {
+    [multisigSteps[0]]: createDefaultMultisigInit(),
+    [multisigSteps[1]]: createDefaultMembers(),
+    [multisigSteps[2]]: createDefaultFund(),
+  }
+}
+
+function createWizardState() {
+  const step = signal(0)
+  const formData = computed(() => createDefaultFormData())
+
+  return {
+    step,
+    formData: formData.value,
+  }
+}
+
+export const wizardState = createWizardState()
+
+export function useWizardNavigation() {
+  const { step } = wizardState
+
+  function goNext() {
+    step.value = step.value + 1
+  }
+
+  function goPrev() {
+    step.value = step.value - 1
+  }
+  return {
+    goNext,
+    goPrev,
+  }
+}
+
+export function useWizardFormDataStep<T extends MultisigEntities>() {
+  const { formData, step } = wizardState
+  const stepIdx = multisigSteps[step.value] as keyof FormData
+  const formDataStep = formData[stepIdx]
+
+  const updateFormDataStep = (formDataNew: T) => {
+    formDataStep.value = formDataNew
+  }
+
+  return {
+    updateFormDataStep,
+    formDataStep: formDataStep.value as T,
+  }
+}
+
+export function Wizard({ children }: { children: ComponentChildren }) {
+  return <>{toChildArray(children)[wizardState.step.value]}</>
+}
