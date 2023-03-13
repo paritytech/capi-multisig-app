a02ce3dc865526dc0c4ed20bb42f5181a8650bbd
Harry Solovay <harrysolovay@gmail.com>
2023-03-13 15:10:25 -0400

    import dotenv

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,7 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone"
 import cors from "cors"
 import { router } from "./index.js"
+import "dotenv"
 
 const server = createHTTPServer({
   router,
