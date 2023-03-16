8be19a724df69ac31948dd8af94b0758bc21e882
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-16 21:12:45 +0700

    compatible with react hook form

--- www/src/components/AddressInput.tsx
+++ www/src/components/AddressInput.tsx
@@ -1,57 +1,39 @@
 import { clsx } from 'clsx'
-import { JSX } from 'preact/jsx-runtime'
+import type { JSX } from 'preact/jsx-runtime'
 import { Identicon } from './identicon/Identicon.js'
 import { isValidAddress } from '../util/isValidAddress.js'
 import { VoidIdenticon } from './identicon/VoidIdenticon.js'
+import { ForwardedRef, forwardRef } from 'preact/compat'
 
-interface Props {
-  id?: string
-  name?: string
-  placeholder: string
-  label: string
-  value: string
-  onChange: (e: JSX.TargetedEvent<HTMLInputElement, Event>) => void
-}
-
-export function AddressInput({
-  id,
-  name,
-  placeholder,
-  label,
+export const AddressInput = forwardRef(({
   value,
-  onChange,
   ...props
-}: Props) {
-  const isValid = isValidAddress(value)
+}:
+  Omit<JSX.HTMLAttributes<HTMLInputElement>, 'value'> & { value?: string },
+  ref: ForwardedRef<HTMLInputElement>
+) => {
+  const isValid = value && isValidAddress(value)
 
   return (
-    <div className="flex flex-col gap-2 w-full">
-      <label htmlFor={name} className="block text-input-text">
-        {label}
-      </label>
-
-      <div className="relative rounded-lg shadow-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center p-3">
-          {isValid ? (
-            <Identicon value={value} size={24} />
-          ) : (
-            <VoidIdenticon className="h-6 w-6" />
-          )}
-        </div>
-
-        <input
-          type="text"
-          name={name}
-          id={id}
-          value={value}
-          className={clsx(
-            'pl-10 bg-input-bg block w-full rounded-lg border-input-border focus:ring-1 focus:ring-input-focus focus:border-input-focus',
-          )}
-          placeholder={placeholder}
-          onChange={onChange}
-          {...props}
-        />
+    <div className="relative rounded-lg shadow-sm">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center p-3">
+        {isValid ? (
+          <Identicon value={value} size={24} />
+        ) : (
+          <VoidIdenticon className="h-6 w-6" />
+        )}
       </div>
+
+      <input
+        ref={ref}
+        value={value}
+        type="text"
+        className={clsx(
+          'pl-10 h-12 block w-full rounded-lg border-input-border',
+          'focus:outline-none focus-visible:ring focus-visible:ring-cyan-700 focus-visible:ring-opacity-75 focus-visible:ring-offset focus-visible:ring-offset-cyan-700',
+        )}
+        {...props}
+      />
     </div>
   )
-}
+})
