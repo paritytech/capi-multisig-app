c4934ad0f20d653045f7b3490b83ffdf468cd438
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-21 20:04:41 +0700

    IconChevronLeft & IconChevronRight

---
+++ www/src/components/icons/IconChevronRight.tsx
@@ -0,0 +1,16 @@
+import type { JSX } from 'preact/jsx-runtime'
+
+export const IconChevronRight = (
+  props: JSX.IntrinsicAttributes & JSX.SVGAttributes<SVGSVGElement>,
+) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M9.70492 6L8.29492 7.41L12.8749 12L8.29492 16.59L9.70492 18L15.7049 12L9.70492 6Z"
+      fill-opacity="0.89"
+    />
+  </svg>)
