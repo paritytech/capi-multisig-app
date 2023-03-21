04c2a10ff0e2861429ee128a6da1b9be0cd2f704
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-21 20:04:05 +0700

    fix: card width

--- www/src/components/CenteredCard.tsx
+++ www/src/components/CenteredCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 export function CenteredCard({ children }: Props) {
   return (
     <div className="flex items-center justify-center pt-16">
-      <div className="max-w-[640px] bg-white py-6 px-4 border border-border rounded-lg shadow-card">{children}</div>
+      <div className="w-3/5 max-w-[640px] bg-white py-6 px-4 border border-border rounded-lg shadow-card">{children}</div>
     </div>
   )
 }
