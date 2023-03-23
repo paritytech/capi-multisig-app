d7b795cf95cd945cc52c7273663048b5c60fddb9
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-23 17:55:03 +0700

    continue

--- www/src/components/Capi.tsx
+++ www/src/components/Capi.tsx
@@ -1,18 +1,20 @@
-import { System } from "@capi/polkadot_westend" // Westend
-// import { System } from "@capi/polkadot" // Polkadot
-import { alice } from "capi"
+// import { System } from "@capi/polkadot"
+import { ChainRune, WsConnection } from "capi"
 import { useEffect } from "preact/hooks"
 
 export function Capi() {
   useEffect(() => {
     const fetchBalance = async () => {
       // https://github.com/paritytech/capi/blob/main/examples/balance.ts
-      try {
-        const result = await System.Account.value(alice.publicKey).run()
-        console.log(result)
-      } catch (error) {
-        console.error(error)
-      }
+      const chain = ChainRune.from(WsConnection, "wss://rpc.polkadot.io")
+
+      const accountInfo = await chain
+        .pallet("System")
+        .storage("Account")
+        .entryPage(10, null)
+        .run()
+
+      console.log(accountInfo)
     }
 
     fetchBalance()
