5610cecb5b6484f4b63419c5309884a3b08e5270
tamrai <nick.zbiegien@parity.io>
2023-03-23 16:07:23 +0700

    tweaks

--- www/src/components/Table.tsx
+++ www/src/components/Table.tsx
@@ -1,11 +1,11 @@
 import type { VNode } from "preact"
 import { toChildArray } from "preact"
 
-type FeesTableProps = {
+type TableProps = {
   children: VNode<ItemProps>[] | VNode<ItemProps>
 }
 
-export function FeesTable({ children }: FeesTableProps) {
+export function Table({ children }: TableProps) {
   const totalFee = (toChildArray(children) as VNode<ItemProps>[]).reduce(
     (total: number, { props: { fee } }) => total + fee,
     0,
@@ -28,15 +28,16 @@ export function FeesTable({ children }: FeesTableProps) {
 type ItemProps = {
   name: string
   fee: number
+  unit: string
 }
 
-function Item({ name, fee }: ItemProps) {
+function Item({ name, fee, unit }: ItemProps) {
   return (
     <div class="pb-4 px-6 flex justify-between">
       <dt>{name}</dt>
-      <dd>{fee} DOT</dd>
+      <dd>{fee} {unit}</dd>
     </div>
   )
 }
 
-FeesTable.Item = Item
+Table.Item = Item
--- www/src/pages/dashboard.tsx
+++ www/src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 import { Button } from "../components/Button.js"
 import { CenteredCard } from "../components/CenteredCard.js"
-import { FeesTable } from "../components/FeesTable.js"
 import { IconPlus } from "../components/icons/IconPlus.js"
+import { Table } from "../components/Table.js"
 import { trpc } from "../trpc.js"
 import { Page } from "./templates/base.js"
 
@@ -14,11 +14,11 @@ export function Dashboard() {
     <Page>
       <CenteredCard>
         {/* TODO: Preview example - remove before merge  */}
-        <FeesTable>
-          <FeesTable.Item name="Deposit base" fee={20.088} />
-          <FeesTable.Item name="Threshold" fee={0.064} />
-          <FeesTable.Item name="Vault pure proxy" fee={20} />
-        </FeesTable>
+        <Table>
+          <Table.Item name="Deposit base" fee={20.088} unit="DOT" />
+          <Table.Item name="Threshold" fee={0.064} unit="DOT" />
+          <Table.Item name="Vault pure proxy" fee={20} unit="DOT" />
+        </Table>
         <Link to="/create-multisig" className="block mt-4">
           <div className="flex flex-col gap-4 wrap">
             <Button iconLeft={<IconPlus />}>New multisig</Button>
