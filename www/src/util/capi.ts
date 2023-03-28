0398a89dcd4b4e8b3dd9cdfde9d1bb54a6f90488
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-03-28 16:59:46 +0700

    fix

--- www/src/util/capi.ts
+++ www/src/util/capi.ts
@@ -1,12 +1,10 @@
-import { System } from "@capi/polkadot_westend"
-import { ss58 } from "capi"
+import { System } from "@capi/westend";
+import { ss58 } from "capi";
 
 export async function getBalance(address: string) {
-  const addressPubKey = ss58.decode(address)[1]
+  const addressPubKey = ss58.decode(address)[1];
 
-  const balance = await System.Account.value(
-    addressPubKey,
-  ).run()
+  const balance = await System.Account.value(addressPubKey).run();
 
-  return balance ? balance.data.free : 0
+  return balance ? balance.data.free : 0;
 }
