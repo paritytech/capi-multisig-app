a9cf216a65469661496d377ebb2ee25ec6496b4e
dnlklmn <pelicansinberlin@gmail.com>
2023-04-06 11:11:52 +0200

    refine generation

--- www/sd.config.cjs
+++ www/sd.config.cjs
@@ -16,110 +16,110 @@ const sdConfig = makeSdTailwindConfig({
   }
 })
 
-sdConfig.platforms['css'] = {
-  transformGroup: 'css',
-  buildPath: 'src/theme/try/',
-  files: [
-    {
-      destination: 'tailwind.css',
-      format: 'css/variables'
-    }
-  ]
-}
+// sdConfig.platforms['css'] = {
+//   transformGroup: 'css',
+//   buildPath: 'src/theme/try/',
+//   files: [
+//     {
+//       destination: 'tailwind.css',
+//       format: 'css/variables'
+//     }
+//   ]
+// }
 
-const StyleDictionary = StyleDictionaryModule.extend(sdConfig)
-StyleDictionary.buildAllPlatforms()
+const StyleDictionaryWithTailwind = StyleDictionaryModule.extend(sdConfig)
+StyleDictionaryWithTailwind.buildAllPlatforms()
 
-// const StyleDictionaryPackage = require("style-dictionary");
-// const { registerTransforms } = require("@tokens-studio/sd-transforms");
-// const StyleDictionary = require("style-dictionary");
+const StyleDictionaryPackage = require("style-dictionary");
+const { registerTransforms } = require("@tokens-studio/sd-transforms");
+const StyleDictionary = require("style-dictionary");
 
-// registerTransforms(StyleDictionary);
+registerTransforms(StyleDictionary);
 
-// StyleDictionaryPackage.registerFormat({
-//   name: "css/variables",
-//   formatter: function (dictionary) {
-//     return `${this.selector} {\n${dictionary.allProperties
-//       .map((prop) => `  --${prop.name}: ${prop.value};`)
-//       .join("\n")}\n}`;
-//   },
-// });
+StyleDictionaryPackage.registerFormat({
+  name: "css/variables",
+  formatter: function (dictionary) {
+    return `${this.selector} {\n${dictionary.allProperties
+      .map((prop) => `  --${prop.name}: ${prop.value};`)
+      .join("\n")}\n}`;
+  },
+});
 
-// StyleDictionaryPackage.registerFormat({
-//   name: "tailwind/colors",
-//   formatter: function (dictionary) {
-//     return `${this.selector} {\n${dictionary.allProperties
-//       .map((prop) => `"${prop.name}": "${prop.value}",`)
-//       .join("\n")}\n}`;
-//   },
-// });
+StyleDictionaryPackage.registerFormat({
+  name: "tailwind/colors",
+  formatter: function (dictionary) {
+    return `${this.selector} {\n${dictionary.allProperties
+      .map((prop) => `"${prop.name}": "${prop.value}",`)
+      .join("\n")}\n}`;
+  },
+});
 
-// StyleDictionaryPackage.registerFormat({
-//   name: "tailwind/fontFamily",
-//   formatter: function (dictionary) {
-//     return `${this.selector} {\n${dictionary.allProperties
-//       .map(
-//         (prop) =>
-//           `"${prop.name.substring(0, prop.name.indexOf("-font-family"))}": "${
-//             prop.value
-//           }",`
-//       )
-//       .join("\n")}\n}`;
-//   },
-// });
+StyleDictionaryPackage.registerFormat({
+  name: "tailwind/fontFamily",
+  formatter: function (dictionary) {
+    return `${this.selector} {\n${dictionary.allProperties
+      .map(
+        (prop) =>
+          `"${prop.name.substring(0, prop.name.indexOf("-font-family"))}": "${
+            prop.value
+          }",`
+      )
+      .join("\n")}\n}`;
+  },
+});
 
-// StyleDictionaryPackage.registerTransform({
-//   name: "size/px",
-//   type: "value",
-//   transitive: true,
+StyleDictionaryPackage.registerTransform({
+  name: "size/px",
+  type: "value",
+  transitive: true,
 
-//   matcher: (token) => ["fontWeights"].includes(token.type),
-//   transformer: (token) => (token.value === "Regular" ? 400 : 700),
+  matcher: (token) => ["fontWeights"].includes(token.type),
+  transformer: (token) => (token.value === "Regular" ? 400 : 700),
 
-//   matcher: (token) =>
-//     ["fontSizes", "dimension", "borderRadius", "spacing"].includes(token.type),
-//   transformer: (token) => parseFloat(token.value) + "px",
-// });
+  matcher: (token) =>
+    ["fontSizes", "dimension", "borderRadius", "spacing"].includes(token.type),
+  transformer: (token) => parseFloat(token.value) + "px",
+});
 
-// ["light", "dark", "semantic"].map((theme) => {
-//   const themeVariables = StyleDictionary.extend({
-//     source: [`src/theme/${theme}.json`],
-//     include: [`src/theme/global.json`],
-//     platforms: {
-//       web: {
-//         transformGroup: "tokens-studio",
-//         transforms: ["attribute/cti", "name/cti/kebab", "size/px"],
-//         buildPath: "src/theme/try/",
-//         files: [
-//           {
-//             destination: `${theme}.css`,
-//             format: "css/variables",
-//             selector:
-//               theme === "dark" ? `:root[data-theme="${theme}"]` : `:root`,
-//             filter: ({ isSource }) => {
-//               return isSource;
-//             },
-//           },
-//           {
-//             destination: `${theme}.js`,
-//             format: "tailwind/colors",
-//             selector: "module.exports = ",
-//             filter: ({ isSource }) => {
-//               return isSource;
-//             },
-//           },
-//           {
-//             destination: `${theme}.js`,
-//             format: "tailwind/fontFamily",
-//             selector: "module.exports = ",
-//             filter: (token) => {
-//               return token.type === "fontFamilies";
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   });
-//   themeVariables.cleanAllPlatforms();
-//   themeVariables.buildAllPlatforms();
-// });
+["light", "dark", "semantic", "global"].map((theme) => {
+  const themeVariables = StyleDictionary.extend({
+    source: [`src/theme/${theme}.json`],
+    include: [`src/theme/global.json`],
+    platforms: {
+      web: {
+        transformGroup: "tokens-studio",
+        transforms: ["attribute/cti", "name/cti/kebab", "size/px"],
+        buildPath: "src/theme/try/",
+        files: [
+          {
+            destination: `${theme}.css`,
+            format: "css/variables",
+            selector:
+              theme === "dark" ? `:root[data-theme="${theme}"]` : `:root`,
+            filter: ({ isSource }) => {
+              return isSource;
+            },
+          },
+          // {
+          //   destination: `${theme}.js`,
+          //   format: "tailwind/colors",
+          //   selector: "module.exports = ",
+          //   filter: ({ isSource }) => {
+          //     return isSource;
+          //   },
+          // },
+          // {
+          //   destination: `${theme}.js`,
+          //   format: "tailwind/fontFamily",
+          //   selector: "module.exports = ",
+          //   filter: (token) => {
+          //     return token.type === "fontFamilies";
+          //   },
+          // },
+        ],
+      },
+    },
+  });
+  themeVariables.cleanAllPlatforms();
+  themeVariables.buildAllPlatforms();
+});
