ada69975ad29b5d707dc214c7a48773aa1934e71
Ryan Lee <ryan@parity.io>
2023-04-27 09:52:49 -0400

    update capi config version

--- capi.config.ts
+++ capi.config.ts
@@ -3,7 +3,7 @@ import { binary, Config } from "capi"
 const polkadot = binary("polkadot", "v0.9.38")
 
 export const config: Config = {
-  server: "https://capi.dev/@v0.1.0-beta.35/",
+  server: "https://capi.dev/@v0.1.0-beta.36/",
   chains: {
     westend: {
       url: "wss://westend-rpc.polkadot.io/",
