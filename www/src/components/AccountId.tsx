55bc22c93f5e8a4398ae2dfe006fcb24c14bc1f0
peetzweg/ <phil.czek@gmail.com>
2023-05-05 10:16:48 +0200

    makes all keys `WalletAccount` optional, except `address`

--- www/src/components/AccountId.tsx
+++ www/src/components/AccountId.tsx
@@ -1,25 +1,36 @@
 import { WalletAccount } from "@talisman-connect/wallets"
+import { clsx } from "clsx"
 import { Identicon } from "../components/identicon/Identicon.js"
 import { shortAddress } from "../util/address.js"
 
 interface Props {
-  account: WalletAccount | undefined
+  account?: Partial<WalletAccount> & { address: string }
+  shortenAddress?: boolean
 }
 
-export function AccountId({ account }: Props) {
-  return account
-    ? (
-      <div className="flex">
-        <Identicon
-          size={24}
-          value={account.address}
-          className="mr-2"
-        />
+export function AccountId({ account, shortenAddress = true }: Props) {
+  if (!account) return null
+
+  return (
+    <div className="flex">
+      <Identicon
+        size={24}
+        value={account.address}
+        className="mr-2"
+      />
+
+      {account.name && (
         <span className="font-bold mr-2">
           {account.name}
         </span>
-        <span>{shortAddress(account.address)}</span>
-      </div>
-    )
-    : null
+      )}
+
+      <span
+        title={account.address}
+        className={clsx({ truncate: shortAddress })}
+      >
+        {shortenAddress ? shortAddress(account.address) : account.address}
+      </span>
+    </div>
+  )
 }
