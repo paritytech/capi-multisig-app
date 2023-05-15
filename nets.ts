59c35f5d88638b256ea96675ecaefef554404de8
Ryan Lee <ryan@parity.io>
2023-05-15 12:57:21 -0400

    update nets

--- nets.ts
+++ nets.ts
@@ -1,6 +1,16 @@
-import { net } from "capi"
+import { bins, net } from "capi";
+
+const bin = bins({
+  polkadot: ["polkadot-fast", "v0.9.38"],
+});
+
+export const westendDev = net.dev({
+  bin: bin.polkadot,
+  chain: "westend-dev",
+});
 
 export const westend = net.ws({
   url: "wss://westend-rpc.polkadot.io/",
+  targets: { dev: westendDev },
   version: "v0.9.42",
-})
+});
