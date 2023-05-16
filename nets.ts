4fe7ab386c34e3091397c27df2f82f792e6d8caf
Ryan Lee <ryan@parity.io>
2023-05-16 09:41:48 -0400

    revert to 0.1.0-beta.38

--- nets.ts
+++ nets.ts
@@ -1,16 +1,6 @@
-import { bins, net } from "capi"
-
-const bin = bins({
-  polkadot: ["polkadot-fast", "v0.9.38"],
-})
-
-export const westendDev = net.dev({
-  bin: bin.polkadot,
-  chain: "westend-dev",
-})
+import { net } from "capi";
 
 export const westend = net.ws({
   url: "wss://westend-rpc.polkadot.io/",
-  targets: { dev: westendDev },
   version: "v0.9.42",
-})
+});
