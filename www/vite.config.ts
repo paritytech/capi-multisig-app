b25e0295c9e0d5ce45fec379dc6c2cca8706cae4
peetzweg/ <phil.czek@gmail.com>
2023-05-31 09:58:00 +0200

    makes fs/promises and steam external depedencies in vite config

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -4,14 +4,17 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [preact(), tsConfigPaths()],
-  build: { target: "esnext" },
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      external: ["node:fs/promises", "node:stream"],
+    },
+  },
   optimizeDeps: {
     esbuildOptions: { target: "es2022" },
   },
   define: {
-    "process.env": {
-      CAPI_SERVER: process.env.CAPI_SERVER,
-      CAPI_TARGET: process.env.CAPI_TARGET,
-    },
+    "process.env.CAPI_SERVER": process.env.CAPI_SERVER,
+    "process.env.CAPI_TARGET": process.env.CAPI_TARGET,
   },
 });
