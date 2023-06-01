08860d9d691b88f49895fd92635f4c9b3ccf8d73
peetzweg/ <phil.czek@gmail.com>
2023-06-01 14:00:29 +0200

    adapt vite config as suggested by @kratico

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,11 +1,20 @@
-import { preact } from "@preact/preset-vite"
-import { defineConfig } from "vite"
-import tsConfigPaths from "vite-tsconfig-paths"
+import { preact } from "@preact/preset-vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [preact(), tsConfigPaths()],
-  build: { target: "esnext" },
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      external: ["node:fs/promises", "node:stream"],
+    },
+  },
   optimizeDeps: {
     esbuildOptions: { target: "es2022" },
   },
-})
+  define: {
+    "process.env.CAPI_SERVER": process.env.CAPI_SERVER,
+    "process.env.CAPI_TARGET": process.env.CAPI_TARGET,
+  },
+});
