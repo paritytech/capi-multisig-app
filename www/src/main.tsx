0b5cefda1e9dd2dc292666324bdfbaa7bb07c00a
peetzweg/ <phil.czek@gmail.com>
2023-06-01 11:39:56 +0200

    adds an individual multisig page

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,20 +1,20 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render } from "preact";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import { CreateMultisig } from "./pages/create-multisig.js";
-import { Dashboard } from "./pages/dashboard.js";
-import { PageError } from "./pages/error.js";
-import { Multisig } from "./pages/multisig.js";
-import { NewTransaction } from "./pages/new-transaction.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { render } from "preact"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./index.css"
+import { CreateMultisig } from "./pages/create-multisig.js"
+import { Dashboard } from "./pages/dashboard.js"
+import { PageError } from "./pages/error.js"
+import { Multisig } from "./pages/multisig.js"
+import { NewTransaction } from "./pages/new-transaction.js"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 function Main() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />;
     </QueryClientProvider>
-  );
+  )
 }
 
 const router = createBrowserRouter([
@@ -38,6 +38,6 @@ const router = createBrowserRouter([
     element: <Multisig />,
     errorElement: <PageError />,
   },
-]);
+])
 
-render(<Main />, document.getElementById("app")!);
+render(<Main />, document.getElementById("app")!)
