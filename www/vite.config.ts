1b6940ac675664e5cbeaeedf0a13ceff6eb958bd
peetzweg/ <phil.czek@gmail.com>
2023-06-02 10:24:23 +0200

    adds back in capi required env vars...

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     esbuildOptions: { target: "es2022" },
   },
   define: {
-    process: { env: {} },
+    "process.env.CAPI_SERVER": process.env.CAPI_SERVER,
+    "process.env.CAPI_TARGET": process.env.CAPI_TARGET,
   },
 });
