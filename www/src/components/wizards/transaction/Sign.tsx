a789bc1e559fc77cc74bc525c2c7936e7ebe9caf
peetzweg/ <phil.czek@gmail.com>
2023-06-05 15:37:32 +0200

    store callData for signed multisig extrinisics

--- www/src/components/wizards/transaction/Sign.tsx
+++ www/src/components/wizards/transaction/Sign.tsx
@@ -8,6 +8,7 @@ import {
 
 import { useState } from "preact/hooks"
 import { useNavigate } from "react-router-dom"
+import { storeCall } from "../../../util/local-storage.js"
 import { AccountId } from "../../AccountId.js"
 import { Button } from "../../Button.js"
 import { IconTrash } from "../../icons/IconTrash.js"
@@ -49,6 +50,7 @@ export function TransactionSign() {
 
     ratifyCall
       .run()
+      .then(() => storeCall(call))
       .then(() => {
         setSubmitting(false)
         navigate("/")
