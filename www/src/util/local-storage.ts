c6cf795fb847ff691fb105536f57576371a2010c
peetzweg/ <phil.czek@gmail.com>
2023-06-05 15:37:02 +0200

    adds temporary functions to persist encoded callData in browser

--- www/src/util/local-storage.ts
+++ www/src/util/local-storage.ts
@@ -1,4 +1,6 @@
+import { $runtimeCall, Westend } from "@capi/westend"
 import { Wallet, WalletAccount } from "@talisman-connect/wallets"
+import { ExtrinsicRune, hex, Rune } from "capi"
 import { isSetup, Setup } from "common"
 
 export function retrieveStored(key: "defaultAccount"): WalletAccount | undefined
@@ -15,7 +17,7 @@ export function retrieveStored(key: "defaultExtension" | "defaultAccount") {
   return
 }
 
-// TODO temporary solution to persist setups until saved in backend db
+// TODO temporary solution to persist SETUPS until saved in backend db
 export function getStoredSetups(member: string): Setup[] {
   const storedSetups = localStorage.getItem(member)
   if (!storedSetups) return []
@@ -29,7 +31,7 @@ export function getStoredSetups(member: string): Setup[] {
   }
 }
 
-// TODO temporary solution to persist setups until saved in backend db
+// TODO temporary solution to persist SETUPS until saved in backend db
 export function storeSetup(members: string[], setup: Setup) {
   for (const member of members) {
     const setupsOfMember = getStoredSetups(member)
@@ -43,4 +45,24 @@ export function storeSetup(members: string[], setup: Setup) {
     }
     localStorage.setItem(member, JSON.stringify(setupsOfMember))
   }
+}
+
+// TODO temporary solution to persist CALLS until saved in backend db
+export async function storeCall(call: ExtrinsicRune<Westend, never>) {
+  const collection = await Rune.array([
+    call.callHash.access(),
+    call.callData.access(),
+  ]).run()
+  const [hash, data] = collection.map((value) => "0x" + hex.encode(value))
+  if (!hash || !data) throw new Error("Could not store call")
+
+  localStorage.setItem(hash, data)
+}
+
+// TODO temporary solution to persist CALLS until saved in backend db
+export function getCall(callHash: string) {
+  const callData = localStorage.getItem(callHash)
+  if (!callData) return undefined
+
+  return $runtimeCall.decode(hex.decode(callHash))
 }
