97b0df3a05068f6daf9edcfcfc047a177bf96e91
peetzweg/ <phil.czek@gmail.com>
2023-06-07 16:33:44 +0200

    format with dprint

--- www/src/components/wizards/multisig/wizardData.ts
+++ www/src/components/wizards/multisig/wizardData.ts
@@ -1,9 +1,12 @@
-import { Signal, signal } from "@preact/signals";
+import { Signal, signal } from "@preact/signals"
 
-import { z } from "zod";
-import { isValidAddress } from "../../../util/address.js";
+import { z } from "zod"
+import { isValidAddress } from "../../../util/address.js"
 
-export type MultisigData = MultisigInitEntity & MultisigMemberEntity & MultisigFundEntity;
+export type MultisigData =
+  & MultisigInitEntity
+  & MultisigMemberEntity
+  & MultisigFundEntity
 
 export const multisigInitSchema = z
   .object({
@@ -20,9 +23,9 @@ export const multisigInitSchema = z
   .refine((data) => data.memberCount >= data.threshold, {
     path: ["members"],
     message: "The number of members must be greater than the threshold",
-  });
+  })
 
-export type MultisigInitEntity = z.infer<typeof multisigInitSchema>;
+export type MultisigInitEntity = z.infer<typeof multisigInitSchema>
 
 export const multisigMemberSchema = z.object({
   members: z
@@ -36,28 +39,28 @@ export const multisigMemberSchema = z.object({
         .optional()
         .refine((a) => isValidAddress(a?.address), {
           message: "Invalid address",
-        })
+        }),
     )
     .superRefine((val, ctx) => {
-      const addresses = val.map((a) => a?.address);
+      const addresses = val.map((a) => a?.address)
       if (addresses.length !== new Set(addresses).size) {
         ctx.addIssue({
           code: z.ZodIssueCode.custom,
           message: `No duplicates allowed, please choose a different account.`,
           fatal: true,
-        });
+        })
       }
     }),
-});
+})
 
-export type MultisigMemberEntity = z.infer<typeof multisigMemberSchema>;
+export type MultisigMemberEntity = z.infer<typeof multisigMemberSchema>
 
 export const multisigFundSchema = z.object({
   fundingAmount: z.number().min(1, { message: "Fund must be greater than 0" }),
   isFunded: z.boolean().default(false).optional(),
-});
+})
 
-export type MultisigFundEntity = z.infer<typeof multisigFundSchema>;
+export type MultisigFundEntity = z.infer<typeof multisigFundSchema>
 
 const initialValues = {
   name: "",
@@ -66,10 +69,10 @@ const initialValues = {
   members: [],
   fundingAmount: 1,
   isFunded: false,
-};
+}
 
-export const wizardData: Signal<MultisigData> = signal(initialValues);
+export const wizardData: Signal<MultisigData> = signal(initialValues)
 
 export const updateWizardData = (newWizardData: Partial<MultisigData>) => {
-  wizardData.value = { ...wizardData.value, ...newWizardData };
-};
+  wizardData.value = { ...wizardData.value, ...newWizardData }
+}
