aed3201c119b9ed123b69977e4e1004a915961b8
Andreea Eftene <hi@pixelle.ro>
2023-06-09 11:23:47 +0200

    update capi

--- nets.ts
+++ nets.ts
@@ -1,4 +1,13 @@
-import { net } from "capi"
+import { bins, net } from "capi"
+
+const bin = bins({
+  polkadot: ["polkadot-fast", "v0.9.38"],
+})
+
+export const westendDev = net.dev({
+  bin: bin.polkadot,
+  chain: "westend-dev",
+})
 
 export const westend = net.ws({
   url: "wss://westend-rpc.polkadot.io/",
